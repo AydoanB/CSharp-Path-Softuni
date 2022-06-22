46a92f8585c4b3d902c4f948ed31bb95b4a646b3
AydoanB <balakchiev02@gmail.com>
2022-06-23 00:31:51 +0300

    ORM Excercises

---
+++ Entity-Framework-Core/ORM Fundamentals/Excercises/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using Excercises.App.Data.Entities;
+using Excercises.Data;
+
+namespace Excercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Server=.;Database=Excercises;Integrated Security=true;TrustServerCertificate=true";
+
+            SoftUniDbContext dbContext = new SoftUniDbContext(connectionString);
+
+            dbContext.Employees.Add(new Employee
+            {
+                FirstName = "Gosho",
+                LastName = "Inserted",
+                DepartmentId = dbContext.Departments.First().Id,
+                IsEmployed = true,
+            });
+
+            Employee employee = dbContext.Employees.Last();
+            employee.FirstName = "Modified";
+
+            dbContext.SaveChanges(); 
+        }
+    }
+}
