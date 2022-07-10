c3bfa678b747035b77e4442cbdfa841dbd4e08df
AydoanB <balakchiev02@gmail.com>
2022-07-10 09:02:29 +0300

    Category controller implementation

---
+++ Entity-Framework-Core/C# Auto Mapping Objects/01. Fast Food/FastFood.Core/MappingConfiguration/FastFoodProfile.cs
@@ -0,0 +1,28 @@
+﻿using FastFood.Core.ViewModels.Categories;
+
+namespace FastFood.Core.MappingConfiguration
+{
+    using AutoMapper;
+    using FastFood.Models;
+    using ViewModels.Positions;
+
+    public class FastFoodProfile : Profile
+    {
+        public FastFoodProfile()
+        {
+            //Positions
+            this.CreateMap<CreatePositionInputModel, Position>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.PositionName));
+
+            this.CreateMap<Position, PositionsAllViewModel>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.Name));
+
+            //Categories
+            this.CreateMap<CreateCategoryInputModel, Category>()
+                .ForMember(x => x.Name, m => m.MapFrom(s => s.CategoryName));
+
+            this.CreateMap<Category, CreateCategoryInputModel>()
+                .ForMember(x => x.CategoryName, m => m.MapFrom(s => s.Name));
+        }
+    }
+}
